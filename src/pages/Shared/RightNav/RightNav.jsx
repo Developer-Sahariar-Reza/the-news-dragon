18552a77d6eecb6540c3a203c5a33644f3d0b29d
web-developer-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-09-04 14:06:51 +0600

    right nav login button and social media icon done

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import "./RightNav.css";
-
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa6";
 const RightNav = () => {
   return (
     <div>
-      <h1>right nav</h1>
+      <h1>Login With</h1>
+      <div className="login-button-container">
+        <button className="right-nav-google-btn">
+          <FaGoogle /> <span> Login with Google</span>
+        </button>
+        <button className="right-nav-github-btn">
+          <FaGithub /> <span> Login with Github</span>
+        </button>
+      </div>
+
+      <h2>Find Us With</h2>
+      <div className="social-media-container">
+        <li className="social-media-list">
+          <FaFacebook /> <span>Facebook</span>
+        </li>
+        <li className="social-media-list">
+          <FaTwitter /> <span>Twitter</span>
+        </li>
+        <li className="social-media-list">
+          <FaInstagram /> <span>Instagram</span>
+        </li>
+      </div>
     </div>
   );
 };
